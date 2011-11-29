0ce42ea5efdbb2b89510e83b723bd4dedbeb05a0
jan tomsa 1976 <jan.tomsa.1976@gmail.com>
2011-11-29 01:29:15 +0100

    Windows look and feel

--- swing1.clj
+++ swing1.clj
@@ -1,8 +1,11 @@
 (ns swing1
   (:use [clojure.contrib.swing-utils])
-  (:import [javax.swing JFrame JLabel JButton SwingConstants]
+  (:import [javax.swing JFrame JLabel JButton SwingConstants UIManager]
            [java.awt BorderLayout]))
 ; Display the JFrame
+(defn setNativeLookUI []
+  ;; Use the operating system native UI look and feel, do not use the Swing oriented look
+  (. UIManager (setLookAndFeel "com.sun.java.swing.plaf.windows.WindowsLookAndFeel")))
 (let [my-label (JLabel. "Hello World!" SwingConstants/CENTER)
         button (doto (JButton. "Do it!")
                   (add-action-listener
@@ -19,4 +22,5 @@
 	(.setVisible true)
 	(.setAlwaysOnTop true)
      )
+     (setNativeLookUI)
 )
