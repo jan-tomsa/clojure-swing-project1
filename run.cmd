869571ef108a59339321006584240942d4b85705
jan_tomsa <jan_tomsa@localhost>
2011-11-29 01:04:31 +0100

    Initial commit

---
+++ run.cmd
@@ -0,0 +1,2 @@
+@echo off
+clj swing1.clj
---
+++ swing1.clj
@@ -0,0 +1,22 @@
+(ns swing1
+  (:use [clojure.contrib.swing-utils]
+        [clojure.contrib.lazy-seqs :only (fibs)])
+  (:import [javax.swing JFrame JLabel JButton SwingConstants]
+           [java.awt BorderLayout]))
+; Display the JFrame
+(let [my-label (JLabel. "Hello World!" SwingConstants/CENTER)
+        button (doto (JButton. "Do it!")
+                  (add-action-listener
+                    (fn [_]
+                     (.setText my-label
+                      "Hello Clojure!"))))]
+     (doto 
+        (JFrame. "My Swing App")
+        (.setLayout (BorderLayout.))
+	(.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+	(.setSize 200 200)
+	(.add "Center" button)
+	(.add "South" my-label)
+	(.setVisible true)
+     )
+)
