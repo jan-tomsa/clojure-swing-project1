08513f61d83c6fe6f569511eb9627b98763ebfa8
Jan Tomsa 1976 <Jan.Tomsa.1976@gmail.com>
2012-08-27 15:02:25 +0000

    Added JEditorPane, changed close operation -> DISPOSE_ON_CLOSE

--- swing1.clj
+++ swing1.clj
@@ -1,26 +1,28 @@
 (ns swing1
   (:use [clojure.contrib.swing-utils])
-  (:import [javax.swing JFrame JLabel JButton SwingConstants UIManager]
+  (:import [javax.swing JFrame JLabel JButton JEditorPane SwingConstants UIManager]
            [java.awt BorderLayout]))
 ; Display the JFrame
 (defn setNativeLookUI []
   ;; Use the operating system native UI look and feel, do not use the Swing oriented look
   (. UIManager (setLookAndFeel "com.sun.java.swing.plaf.windows.WindowsLookAndFeel")))
 (let [my-label (JLabel. "Hello World!" SwingConstants/CENTER)
-        button (doto (JButton. "Do it!")
+      button (doto (JButton. "Do it!")
                   (add-action-listener
                     (fn [_]
-                     (.setText my-label
-                      "Hello Clojure!"))))]
+                      (.setText my-label "Hello Clojure!")
+		      )))
+      panel (doto (JEditorPane. "text/html" "<h1>Sample text</h1><p>This is sample text</p>") )]
      (doto 
         (JFrame. "My Swing App")
         (.setLayout (BorderLayout.))
-	(.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-	(.setSize 200 200)
-	(.add "Center" button)
-	(.add "South" my-label)
-	(.setVisible true)
-	(.setAlwaysOnTop true)
+        (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE)
+        (.setSize 200 300)
+        (.add "Center" button)
+        (.add "South" my-label)
+        (.add "North" panel)
+        (.setVisible true)
+        (.setAlwaysOnTop true)
      )
      (setNativeLookUI)
 )
