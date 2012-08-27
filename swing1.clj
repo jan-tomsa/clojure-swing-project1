501c357c0ed544626f75fca8c4a736406442cb44
Jan Tomsa 1976 <Jan.Tomsa.1976@gmail.com>
2012-08-27 15:11:27 +0000

    added modification of JEditorPane text to button

--- swing1.clj
+++ swing1.clj
@@ -7,17 +7,19 @@
   ;; Use the operating system native UI look and feel, do not use the Swing oriented look
   (. UIManager (setLookAndFeel "com.sun.java.swing.plaf.windows.WindowsLookAndFeel")))
 (let [my-label (JLabel. "Hello World!" SwingConstants/CENTER)
+      panel (doto (JEditorPane. "text/html" "<h1>Sample text</h1><p>This is sample text</p>") )
       button (doto (JButton. "Do it!")
                   (add-action-listener
                     (fn [_]
                       (.setText my-label "Hello Clojure!")
-		      )))
-      panel (doto (JEditorPane. "text/html" "<h1>Sample text</h1><p>This is sample text</p>") )]
+                      (.setText panel "<h1>Hello Clojure!</h1>")
+                    )))
+     ]
      (doto 
         (JFrame. "My Swing App")
         (.setLayout (BorderLayout.))
         (.setDefaultCloseOperation JFrame/DISPOSE_ON_CLOSE)
-        (.setSize 200 300)
+        (.setSize 300 300)
         (.add "Center" button)
         (.add "South" my-label)
         (.add "North" panel)
