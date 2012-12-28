88c420c86bd5394425b6a62fee6a93713ac8389d
jan_tomsa <jan_tomsa@localhost>
2012-12-28 22:34:11 +0100

    minor improvements

--- clj1/src/basics1.clj
+++ clj1/src/basics1.clj
@@ -1,10 +1,10 @@
 (def my-list ["A" "B" "C" "D" "E" "F" "G" "H" "I"])
 
+(println "Cartesian:")
 (doseq [i my-list, j my-list]
-  (print (str "*" i) )
-  )
+  (print
+    (str i "-" j " ")))
 
-
+(println "\n\nSimple cycle:")
 (doseq [i my-list]
-  (prn i)
-  )
+  (prn i))
